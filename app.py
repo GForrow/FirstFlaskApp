d3ff7f997066c3c83c1529f3d5a2e949a0b2c0b5
GForrow <garyforrowbsc@gmail.com>
2020-07-06 16:46:53 +0100

    Created layout page and implemented it.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello Everybody!!!'
+    return render_template('homepage.html', title='Homepage')
+
+
+@app.route('/about')
+def about():
+    return 'Welcome to the about page.'
 
 
 if __name__ == '__main__':
