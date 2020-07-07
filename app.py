957660eff6ca6909bd8d8dd862496e5d57ac5c10
GForrow <garyforrowbsc@gmail.com>
2020-07-07 10:37:22 +0100

    Added dummy data and dynamically display on homepage.html

--- app.py
+++ app.py
@@ -3,11 +3,25 @@ from flask import render_template
 
 app = Flask(__name__)
 
+dummyData = [
+    {
+        "f_name": "Gary",
+        "l_name": "Forrow",
+        "title": "My First Blog Post",
+        "content": "Blaaah blaaaaahhhhhh",
+    },
+    {
+        "f_name": "Semore",
+        "l_name": "Content",
+        "title": "How much I'd like to see.",
+        "content": "I would like to see more",
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return render_template('homepage.html', title='Homepage')
+    return render_template('homepage.html', title='Homepage', posts=dummyData)
 
 
 @app.route('/about')
